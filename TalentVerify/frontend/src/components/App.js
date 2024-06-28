02744a4913a83432b199685819c1be1ec11665fa
dmahoko263 <110472681+dmahoko263@users.noreply.github.com>
2024-06-28 02:40:39 +0200

    updated react dom

--- TalentVerify/frontend/src/components/App.js
+++ TalentVerify/frontend/src/components/App.js
@@ -1,14 +1,11 @@
-import React, {Component} from "react"
-import { render } from "react-dom"
+import React, { Component } from 'react';
 
-export default class App extends Component{
-    constructor(props){
-        super(props);
-    }
-    render(){
-        return <h1>React frontend set</h1>
-    }
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <h1>React frontend set react 18</h1>;
+  }
 }
-
-const appDiv=document.getElementById("app")
-render(<App />, appDiv)
